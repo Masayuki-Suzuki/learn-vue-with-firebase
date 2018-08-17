1a8de8388e3320013e94780a05baf49f37aff8ce
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-08-17 14:59:44 -0700

    4. 4 ログイン状態のチェック

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,21 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import firebase from 'firebase'
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user)
+      if (user) {
+        this.$store.commit('changeLoginStateLogin')
+      } else {
+        this.$store.commit('changeLoginStateLogout')
+      }
+    })
+  }
+}
 </script>
 
 <style>
